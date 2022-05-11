6ddd834d9c8ac56321eacf459b326d28deb28f08
Woinshet <woinshet1>
2022-05-10 19:14:46 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData = (username, email) => {
+
+    let data = {
+        "name": username,
+        "email": email
+    }
+
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then (res => res.json())
+    .then (data => {
+        document.body.innerHTML = `${data.id}`
+    })
+    .catch ((error) => {
+        //console.log(error)
+        console.log(error.message)
+        document.body.innerHTML = error.message
+    })
+    
+}
